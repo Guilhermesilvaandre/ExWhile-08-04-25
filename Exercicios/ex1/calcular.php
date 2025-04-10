1cf653de7dc22dded5488c085f9071d5fc182345
Guilhermesilvaandre <guilhermeaandre2018@gmail.com>
2025-04-10 18:28:29 -0300

    Ex 1 terminado

--- Exercicios/ex1/calcular.php
+++ Exercicios/ex1/calcular.php
@@ -10,34 +10,34 @@ $um = 0;
 
 echo "<h2>Resultado:</h2>";
 
-    while ($n1 = 0) 
+    while ($n1) 
     {
-        if ($n1 > 100) {
-            $n1 - 100;
+        if ($n1 >= 100) {
+            $n1 -= 100;
             $cem = $cem +1; 
         }
-        else if ($n1 > 50){
-            $n1 - 50;
+        else if ($n1 >= 50){
+            $n1 -= 50;
             $cinquenta++; 
         }
-        else if ($n1 > 20){
-            $n1 - 20;
+        else if ($n1 >= 20){
+            $n1 -= 20;
             $vinte++; 
         }
-        else if ($n1 > 10){
-            $n1 - 10;
+        else if ($n1 >= 10){
+            $n1 -= 10;
             $dez++; 
         }
-        else if ($n1 > 5){
-            $n1 - 5;
+        else if ($n1 >= 5){
+            $n1 -= 5;
             $cinco++; 
         }
-        else if ($n1 > 2){
-            $n1 - 2;
+        else if ($n1 >= 2){
+            $n1 -= 2;
             $dois++; 
         }
         else {
-            $n1 - 1;
+            $n1 -= 1;
             $um++; 
         }
     };
